101cb2e17a8367e0b4ed0c65da2f459e1972d8a9
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-11-04 12:19:35 -0300

    PlayList 04/11/2025

---
+++ Lista_duplamente_encadeada/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "musica.h"
+
+int main(){
+
+    Item * D = criarItem("Aquarela", "Toquinho", 1983);
+    Item * E = criarItem("Romaria", "Renato Teixeira", 1978);
+    Item * F = criarItem("Eu nasci há dez mil anos atrás", "Raul Seixas", 1976);
+    Item * G = criarItem("Calice", "Gilberto Gil e Chico Buarque", 1978);
+    Item * H = criarItem("Tempo Perdido", "Renato Russo", 1986);
+
+
+     // Construindo o encandeamento D,E,F,G,H
+
+     D->Anterior = NULL;
+     D->Posterior = E;
+
+     E->Anterior = D;
+     E->Posterior = F;
+
+     F->Anterior = E;
+     F->Posterior = G;
+
+     G->Anterior = F;
+     G->Posterior = H;
+
+     H->Anterior = G;
+     H->Posterior = NULL;
+
+     // Criando uma lista
+
+     Lista * PlayList = (Lista *) malloc (sizeof(Lista));
+     if (PlayList == NULL){
+        exit(1);
+     }
+
+     PlayList -> Tamanho = 5;
+     PlayList -> Inicio = D;
+     PlayList -> Fim = H;
+
+     exibirInicio(PlayList);
+
+}
---
+++ Lista_duplamente_encadeada/musica.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "musica.h"
+
+Item * criarItem(char * Titulo, char * Autor, int Ano){
+
+    Item * X = (Item *) malloc(sizeof(Item));
+    if (X== NULL){
+        printf("Erro: Nao foi possivel alocar memoria para o Item.\n");
+        return NULL;
+
+    }
+
+    strcpy(X->Titulo, Titulo);
+    strcpy(X->Autor, Autor);
+    X->Ano = Ano;
+    X->Anterior = NULL;
+    X->Posterior = NULL;
+
+    return X;
+
+}
+
+void exibirInicio(Lista * L){
+
+    Item * Atual = L->Inicio;
+
+    for(int i = 0; i < L->Tamanho; i++) {
+printf("%d \t %s \n", i, Atual ->Titulo);
+Atual = Atual->Posterior;
+    }-
+
+    printf("\n");
+}
+
